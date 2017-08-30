e7cb486dc517f21c590c08fbc1cd01841a2d9556
Olga Bazhenova <olgabazhenova07@yandex.ru>
2017-08-30 21:00:51 +0300

    Разделила код на модули с помощью немедленно вызывающихся функций (IIFE)

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  window.util = {
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    },
+    getRandomData: function (array) {
+      var rand = Math.random() * (array.length);
+      rand = Math.floor(rand);
+      return array[rand];
+    }
+  };
+})();
